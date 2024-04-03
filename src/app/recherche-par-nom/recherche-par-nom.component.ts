51492b4c443f4f1db7e87fdde054f09dab25c95e
Charles-Henri St-Mars <ch.stmars@gmail.com>
2024-04-03 23:53:29 +0200

    Recherche de produits par nom avec l'événement onKeyUp

--- src/app/recherche-par-nom/recherche-par-nom.component.ts
+++ src/app/recherche-par-nom/recherche-par-nom.component.ts
@@ -10,31 +10,31 @@ import { ProduitService } from '../services/produit.service';
 export class RechercheParNomComponent implements OnInit {
   nomProduit: string = "";
   produits!: Produit[];
-  // allProduits!: Produit[];
-  // searchTerm!: string;
+  allProduits!: Produit[];
+  searchTerm!: string;
 
   constructor(private produitService: ProduitService) {}
 
   ngOnInit(): void {
     this.produitService.listerProduits().subscribe((prods) => {
       // console.log(prods);
-      // this.allProduits = prods;
+      this.allProduits = prods;
       this.produits = prods;
     });
   }
 
-  rechercherProds() {
+  /* rechercherProds() {
     if (this.nomProduit !== "") {
       this.produitService.rechercherParNom(this.nomProduit).subscribe((prods) => {
         this.produits = prods;
         console.log(prods);
       });
     }    
-  }
+  } */
 
-  /* onKeyUp(filterText: string) {
+  onKeyUp(filterText: string) {
     this.produits = this.allProduits.filter(prod =>
     prod.nomProduit.toLowerCase().includes(filterText.toLowerCase())
     );
-  } */
+  }
 }
